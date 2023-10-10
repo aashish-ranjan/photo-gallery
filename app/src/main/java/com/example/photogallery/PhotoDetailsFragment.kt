8d8c366b796c3d0a1df176bb439f30bb651b6eed
aashish-ranjan <aashishranjan2011@gmail.com>
2023-10-10 18:40:01 +0530

    create photo details fragment

---
+++ app/src/main/java/com/example/photogallery/PhotoDetailsFragment.kt
@@ -0,0 +1,29 @@
+package com.example.photogallery
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.photogallery.databinding.FragmentPhotoDetailsBinding
+
+class PhotoDetailsFragment : Fragment() {
+
+    private var _binding: FragmentPhotoDetailsBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) { "Cannot access binding because it is null. Is the view visible?" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPhotoDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
